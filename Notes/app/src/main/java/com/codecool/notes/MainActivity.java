2f9cabd461c54ce294a0620980b60743e2b593c5
cickib <bellakocsis06@gmail.com>
2017-02-02 20:28:30 +0100

    buttons triggered as expected

--- Notes/app/src/main/java/com/codecool/notes/MainActivity.java
+++ Notes/app/src/main/java/com/codecool/notes/MainActivity.java
@@ -1,13 +1,29 @@
 package com.codecool.notes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "App launched.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAddNew(View view) {
+        Log.d(TAG, "Add new clicked.");
+        Toast.makeText(this, TAG, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSort(View view) {
+        String order = view.getTag().toString();
+        Log.d(TAG, "Sort button clicked. Requested order: " + order);
+        Toast.makeText(this, order, Toast.LENGTH_SHORT).show();
     }
 }
