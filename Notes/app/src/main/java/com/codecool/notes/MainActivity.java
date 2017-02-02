c69284ff58b945c4cf11dd69dc190f4047098941
cickib <bellakocsis06@gmail.com>
2017-02-02 21:13:23 +0100

    new note saved to db

--- Notes/app/src/main/java/com/codecool/notes/MainActivity.java
+++ Notes/app/src/main/java/com/codecool/notes/MainActivity.java
@@ -4,21 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.codecool.notes.dao.DbHelper;
+import com.codecool.notes.model.Note;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName();
+    private DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "App launched.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper = new DbHelper(this);
     }
 
     public void onClickAddNew(View view) {
         Log.d(TAG, "Add new clicked.");
-        Toast.makeText(this, TAG, Toast.LENGTH_SHORT).show();
+        String text = ((EditText) findViewById(R.id.editTextNote)).getText().toString();
+        ((EditText) findViewById(R.id.editTextNote)).setText("");
+        dbHelper.insertNote(new Note(text, new Date()));
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
     public void onClickSort(View view) {
